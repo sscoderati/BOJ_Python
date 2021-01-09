ae3647288fc54cb8ebae09a0d0f57424b4999216
sscoderati <chungup5495@gmail.com>
2021-01-09 08:07:19 +0000

    21.01.09

---
+++ 11653.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+def is_prime(n):
+    for i in range(2, n):
+        if n == 2:
+            return True
+        elif n % i == 0:
+            return False
+        else:
+            continue
+    return True
+
+while n != 1:
+    for i in range(2, n):
+        while is_prime(i) == True and n % i == 0:
+            n /= i
+            print(i)
