d9c99d17497a323c4183de6efca2558499deea9e
sscoderati <chungup5495@gmail.com>
2021-01-23 10:46:16 +0000

    Moved to another repo

--- BOJ_Guide_1/2609.py
+++
@@ -1,7 +0,0 @@
-import math
-def lcm(a, b):
-    return a * b // math.gcd(a, b)
-
-a, b = map(int, input().split())
-print(math.gcd(a, b))
-print(lcm(a, b))
--- BOJ_Guide_1/2869.py
+++
@@ -1,6 +0,0 @@
-import math
-A, B, V = map(int, input().split())
-last = V - A
-real = A - B
-cnt = math.ceil(last / real) + 1
-print(cnt)
